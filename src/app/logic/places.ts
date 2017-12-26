3f138a24de8fadb7d7deaff90911fa61e7ffa2cb
barld.boot <barld.boot@coas.com>
2017-12-26 14:29:33 +0100

    added a few extra places

--- src/app/logic/places.ts
+++ src/app/logic/places.ts
@@ -47,6 +47,141 @@ export const places: Place[] = [
             { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
         ],
         selectionName: 'burgh_haamstede'
-    }
+    },
+    {
+        name: 'Haamstede',
+        emptyings: [
+            { container: 'rest', week: weekType.even, dayOfWeek: dayOfWeek.monday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
+        ],
+        selectionName: 'haamstede'
+    },
+    {
+        name: 'Nieuw-Haamstede',
+        emptyings: [
+            { container: 'rest', week: weekType.even, dayOfWeek: dayOfWeek.monday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
+        ],
+        selectionName: 'nieuw_haamstede'
+    },
+    {
+        name: 'Noordwelle',
+        emptyings: [
+            { container: 'rest', week: weekType.even, dayOfWeek: dayOfWeek.monday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
+        ],
+        selectionName: 'noordwelle'
+    },
+    {
+        name: 'Renesse',
+        emptyings: [
+            { container: 'rest', week: weekType.even, dayOfWeek: dayOfWeek.monday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
+        ],
+        selectionName: 'renesse'
+    },
+    {
+        name: 'Westenschouwen',
+        emptyings: [
+            { container: 'rest', week: weekType.even, dayOfWeek: dayOfWeek.monday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.unEven, dayOfWeek: dayOfWeek.monday }
+        ],
+        selectionName: 'westenschouwen'
+    },
+    {
+        name: 'Brouwershaven',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'brouwershaven'
+    },
+    {
+        name: 'Den Osse',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'den_osse'
+    },
+    {
+        name: 'Ellemeet',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'ellemeet'
+    },
+    {
+        name: 'Kerkwerve (inclusief buitengebied)',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'kerkwerve'
+    },
+    {
+        name: 'Noordgouwe (inclusief buitengebied)',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'noordgouwe'
+    },
+    {
+        name: 'Scharendijke',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'scharendijke'
+    },
+    {
+        name: 'Schuddebeurs',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'schuddebeurs'
+    },
+    {
+        name: 'Serooskerke',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'serooskerke'
+    },
+    {
+        name: 'Zierikzee Poortambacht',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'zierikzee_poortambacht'
+    },
+    {
+        name: 'Zonnemaire',
+        emptyings: [
+            { container: 'rest', week: weekType.unEven, dayOfWeek: dayOfWeek.tuesday },
+            { container: 'plastic', week: weekType.even, dayOfWeek: dayOfWeek.wednesday },
+            { container: 'GFT', week: weekType.even, dayOfWeek: dayOfWeek.tuesday }
+        ],
+        selectionName: 'zonnemaire'
+    },
 ];
 
